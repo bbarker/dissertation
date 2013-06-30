5595eddbce26a48c56aa001d6f588af683416f48
Brandon Barker <beb82@cornell.edu>
2013-06-29 23:28:03 -0400

    Realized Hierarchical model implies CNF; simplified Box 1; began discussion section.

--- auto/dissertation.el
+++ auto/dissertation.el
@@ -2,7 +2,8 @@
  (lambda ()
     (LaTeX-add-bibliographies)
     (LaTeX-add-labels
-     "fig:2F43")
+     "fig:2F43"
+     "ECAssume")
     (TeX-add-symbols
      "ifpdf")
     (TeX-run-style-hooks
