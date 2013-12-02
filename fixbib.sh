2b16fb487216cb8124cda4615817c845840ccaae
bbarker <brandon.barker@gmail.com>
2013-12-01 23:05:59 -0500

    first draft sans results

--- fixbib.sh
+++ fixbib.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Fix Norse \O's :
-perl -pi -e 's/(\\[oO])([^a-zA-Z])/{\1}\2/g' library.bib 
+perl -pi -e 's/(\\[oOI])([^a-zA-Z])/{\1}\2/g' library.bib 
 
 # remove file fields since this varies from system to system
 # and will cause git troubles:
