29cea3c4b40e5d4b12f50636d507d0335a5c04cf
Brandon <brandon.barker@gmail.com>
2014-04-10 14:25:49 -0400

    finished fluxExpCmp and switched away from dvipdfm

--- falconBioinfo/buildall_dvipdfm.sh
+++ falconBioinfo/buildall_dvipdfm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 latex document && bibtex document && latex document && latex supplement \
   && bibtex supplement && latex supplement && latex document && latex supplement\
-  && dvipdf supplement && dvipdf document
+  && dvipdfm supplement && dvipdfm document
